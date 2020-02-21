8b78f60646e1d12060f83ccc3695e201e5b4a98e
yingxinlin <yingxinlindsay@gmail.com>
2020-02-22 01:13:35 +1100

    move data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' A subset of ECCITE-seq data
+#'
+#' Data from Mimitou et al. ECCITE-seq PBMC control sample data, which is a list
+#' of three matrices of RNA, ADT and HTO
+#'
+#'
+#' @references Mimitou, E. P., Cheng, A., Montalbano, A., et al. (2019). Multiplexed detection
+#' of proteins, transcriptomes, clonotypes and CRISPR perturbations in single cells.
+#' Nature Methods, 16(5), 409–412.
+#'
+#' @source Gene Expression Omnibus with the accession code GSE126310.
+"CITEseq_example"
+
+
+#' A subset of Ligand Receptor Pairs
+"lr_pair_subset"
