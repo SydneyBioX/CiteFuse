80273ea032252f619a40f9326a197b0e3c456d6e
YingxinLin <yingxinlindsay@gmail.com>
2021-04-11 14:17:10 +1000

    fix SNF

--- R/runSNF.R
+++ R/runSNF.R
@@ -17,8 +17,7 @@
 #' @param t Number of iterations for the diffusion process.
 #' @param metadata_names A vector indicates the names of metadata returned
 #' @param verbose whether print out the process
-#' @param FDR false discovery rate threshold for highly variable gene selection
-#' @param bio biological component of the variance threshold for highly 
+#' @param topN top highly variable genes are used 
 #' variable gene selection 
 #' (see `modelGeneVar` in `scran` package for more details)
 #'
@@ -31,7 +30,6 @@
 #' @importFrom SingleCellExperiment SingleCellExperiment logcounts
 #' @importFrom Matrix rowSums
 #' @importFrom SummarizedExperiment SummarizedExperiment
-#' @importFrom SNFtool affinityMatrix SNF
 #' @importFrom propr propr
 #' @importFrom stats cor
 #' 
@@ -56,11 +54,10 @@ CiteFuse <- function(sce,
                      K_knn = 20,
                      K_knn_Aff = 30,
                      sigma = 0.45,
-                     t = 20,
+                     t = 10,
                      metadata_names = NULL,
                      verbose = TRUE,
-                     FDR = 0.05,
-                     bio = 0.01) {
+                     topN = 2000) {
 
 
     if (!"logcounts" %in% SummarizedExperiment::assayNames(sce)) {
@@ -71,8 +68,7 @@ CiteFuse <- function(sce,
     if (is.null(W_list)) {
         if (gene_select) {
             hvg <- selectHVG(sce,
-                             FDR = FDR,
-                             bio = bio)
+                             topN = topN)
         } else {
             hvg <- seq_len(nrow(sce))
         }
@@ -103,8 +99,8 @@ CiteFuse <- function(sce,
             cat("Calculating affinity matrix \n")
         }
 
-        W1 <- SNFtool::affinityMatrix(dist_adt, K = K_knn_Aff, sigma = sigma)
-        W2 <- SNFtool::affinityMatrix(dist_rna, K = K_knn_Aff, sigma = sigma)
+        W1 <- affinityMatrix(dist_adt, K = K_knn_Aff, sigma = sigma)
+        W2 <- affinityMatrix(dist_rna, K = K_knn_Aff, sigma = sigma)
         W_list <- list(W1, W2)
     }
 
@@ -112,7 +108,7 @@ CiteFuse <- function(sce,
         cat("Performing SNF  \n")
     }
 
-    W <- SNFtool::SNF(W_list, K = K_knn, t = t)
+    W <- SNF(W_list, K = K_knn, t = t)
 
     if (is.null(metadata_names)) {
         metadata_names <- c("SNF_W", "ADT_W", "RNA_W")
@@ -127,15 +123,172 @@ CiteFuse <- function(sce,
 
 
 
-#' @importFrom scran modelGeneVar
+#' @importFrom scran modelGeneVar getTopHVGs
 
 selectHVG <- function(sce,
-                      FDR = 0.05,
-                      bio = 0.01) {
+                      topN = 2000) {
 
     decomp <- scran::modelGeneVar(sce)
-    decomp <- decomp[order(decomp$bio, decreasing = TRUE), ]
-    hvg <- rownames(decomp)[decomp$FDR < FDR & decomp$bio > bio]
-
+    hvg <- getTopHVGs(decomp, n = topN)
     return(hvg)
+}
+
+
+
+SNF <- function(Wall, K=20, t=20) {
+    # Similarity Network Fusion takes multiple views of a network (Wall) and
+    # fuses them together to create a overall affinity matrix.
+    #
+    # Args:
+    #   Wall: List of matrices, each element is a square symmetric affinity 
+    #       matrix.
+    #   K: Number of neighbors used in the K-nearest neighbours step,??? more details???
+    #   t: Number of iterations for the diffusion process
+    #
+    # Returns:  
+    #   W: Unified similarity graph of all data types in Wall. 
+
+    
+    #Check if Wall names are consistant across all matrices in Wall
+    wall.name.check <- check_wall_names(Wall)
+    wall.names <- dimnames(Wall[[1]])
+    if(!wall.name.check){
+        warning("Dim names not consistent across all matrices in Wall.
+            Returned matrix will have no dim names.")
+    }
+    
+    LW <- length(Wall)
+    
+
+    
+    #Normalize different networks to avoid scale problems.
+    newW <- vector("list", LW)
+    nextW <- vector("list", LW)
+    for(i in 1:LW){
+        Wall[[i]] <- normalize(Wall[[i]])
+        Wall[[i]] <- (Wall[[i]] + t(Wall[[i]]))/2
+    }
+    
+    ### Calculate the local transition matrix. (KNN step?)
+    for(i in 1:LW){
+        newW[[i]] <- (.dominateset(Wall[[i]], K))
+    }
+    
+    #Perform the diffusion for t iterations
+    for (i in 1:t) {
+        for(j in 1:LW){
+            sumWJ <- matrix(0,dim(Wall[[j]])[1], dim(Wall[[j]])[2])
+            for(k in 1:LW){
+                if(k != j) {
+                    sumWJ <- sumWJ + Wall[[k]]
+                }
+            }
+            nextW[[j]] <- newW[[j]] %*% (sumWJ/(LW-1)) %*% t(newW[[j]])
+        }
+        
+        #Normalize each new obtained networks.
+        for(j in 1 : LW){
+            Wall[[j]] <- normalize(nextW[[j]])
+            Wall[[j]] <- (Wall[[j]] + t(Wall[[j]]))/2;
+        }
+    }
+    
+    # Construct the combined affinity matrix by summing diffused matrices
+    W <- matrix(0, nrow(Wall[[1]]), ncol(Wall[[1]]))
+    for(i in 1:LW){
+        W <- W + Wall[[i]]
+    }
+    
+    W <- W/LW
+    W <- normalize(W)
+    W <- (W + t(W)) / 2
+    
+    if(wall.name.check){
+        dimnames(W) <- wall.names
+    } 
+    
+    return(W)  
+}
+
+
+affinityMatrix <- function(diff,K=20,sigma=0.5) {
+    # Computes the affinity matrix for a given distance matrix
+    # 
+    # Args:
+    #   diff: Distance matrix 
+    #   K: Number of nearest neighbours to sparsify similarity
+    #   sigma: Variance for local model
+    #
+    # Returns:
+    #   Affinity matrix using exponential similarity kernel scaled by k nearest
+    #       neighbour average similarity
+    #
+    
+    N <- nrow(diff)
+    
+    diff <- (diff + t(diff)) / 2
+    diag(diff) <- 0
+    sortedColumns <- as.matrix(t(apply(diff,2,sort)))
+    
+    finiteMean <- function(x){
+        return(mean(x[is.finite(x)]))
+    }
+    means <- apply(sortedColumns[,1:K+1],1,finiteMean)+.Machine$double.eps;
+    
+    avg <- function(x,y){
+        return((x+y)/2)
+    }
+    Sig <- outer(means,means,avg)/3*2 + diff/3 + .Machine$double.eps;
+    Sig[Sig <= .Machine$double.eps] <- .Machine$double.eps
+    densities <- dnorm(diff, 0, sigma*Sig, log = FALSE)
+    
+    W <- (densities + t(densities)) / 2
+    return(W)
+}
+
+
+
+.dominateset <- function(xx,KK=20) {
+    ###This function outputs the top KK neighbors.	
+    
+    zero <- function(x) {
+        s = sort(x, index.return=TRUE)
+        x[s$ix[1:(length(x)-KK)]] = 0
+        return(x)
+    }
+    normalize <- function(X) X / rowSums(X)
+    A = matrix(0,nrow(xx),ncol(xx));
+    for(i in 1:nrow(xx)){
+        A[i,] = zero(xx[i,]);
+        
+    }
+    
+    
+    return(normalize(A))
+}
+
+
+
+check_wall_names <- function(Wall){
+    # Checks if dimnames are consistant across all matrices in Wall
+    #   #Move to internal functions?
+    # Args:
+    #   Wall: List of matrices
+    # Returns:
+    #   logical: True/False indicator of dimnames equivalence
+    name_match <- function(names_A, names_B){
+        return(identical(dimnames(names_A), dimnames(names_B)))
+    }
+    
+    return(all(unlist(lapply(Wall, FUN=name_match, Wall[[1]]))))
+}
+
+#Normalization method for affinity matrices
+normalize <- function(X){
+    row.sum.mdiag <- rowSums(X) - diag(X) 
+    #If rowSumx(X) == diag(X), set row.sum.mdiag to 1 to avoid div by zero
+    row.sum.mdiag[row.sum.mdiag == 0] <- 1   
+    X <- X/(2*(row.sum.mdiag))
+    diag(X) <- 0.5
+    return(X)
 }
